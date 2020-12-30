7acb0c855d59675da1bcb5009f57f26cd1253cc2
kimGH94 <kgassd002@gmail.com>
2020-12-30 16:13:13 +0900

    1

--- matjib/src/main/java/controller/MainController.java
+++ matjib/src/main/java/controller/MainController.java
@@ -1,22 +1,74 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import dao.LoginDao;
+import model.User;
+
 @Controller
 public class MainController {
-	@RequestMapping(value= "/jsp/menutop.html")
-	public ModelAndView menutop() {
-		ModelAndView mav = new ModelAndView("jsp/template");
-//		mav.addObject("");
-		mav.addObject("BODY","menu.jsp");
+	@Autowired
+	private LoginDao loginDao;
+	
+	
+	
+	//고객센터 매핑//
+	@RequestMapping(value="/jsp/qnacenter.html",
+			method=RequestMethod.GET)
+	public ModelAndView qnacenter() {
+		ModelAndView mav = new ModelAndView(
+				"jsp/template");
+		mav.addObject("BANNER","banner_qnacenter.jsp");
+		mav.addObject("BODY","qnacenter.jsp");
 		return mav;
 	}
-	@RequestMapping(value= "/jsp/template.html")
-	public ModelAndView main() {
-		ModelAndView mav = new ModelAndView("jsp/template");
+	
+	
+	
+	// 랭킹 세부화면으로 이동 //
+	@RequestMapping(value="/main/ranking.html",
+			method=RequestMethod.GET)
+	public ModelAndView ranking() {
+		ModelAndView mav = new ModelAndView(
+				"jsp/template");
+		mav.addObject("BANNER","banner_ranking.jsp");
+		mav.addObject("BODY","ranking.jsp");		
+		return mav;
+	}
+	
+	
+	//회원 가입으로 이동
+	@RequestMapping(value="/jsp/userentry.html",
+			method=RequestMethod.GET)
+	public ModelAndView entryForm() {
+		ModelAndView mav = new ModelAndView(
+				"jsp/template");
+		mav.addObject("user",new User());
+		mav.addObject("BANNER","banner_entry.jsp");
+		mav.addObject("BODY","userentry.jsp");		
+		return mav;		
+	}
+	
+	// BODY안에 4사진 
+	@RequestMapping(value="/jsp/main.html",
+			method=RequestMethod.GET)
+	public ModelAndView main(String BODY) {
+		ModelAndView mav = new ModelAndView(
+				"jsp/template");
+		mav.addObject("BODY","main.jsp");		
+		return mav;
+	}
+	
+		
+	// 첫화면 매핑 (Index 와 template)
+	@RequestMapping(value="/main/template.html",method=RequestMethod.GET)
+	public ModelAndView template() {
+		ModelAndView mav = new ModelAndView(
+				"jsp/template");		
 		return mav;
 	}
 }
